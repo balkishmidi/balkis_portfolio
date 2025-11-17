810e6d46a009be6265de2a09b8d15741502fdcfe
Balkis Hmidi <balkisshmidi1999@gmail.com>
2025-11-18 00:16:11 +0100

    fix vercel

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // important for SPA routing on Vercel
-  build: {
-    outDir: 'dist', // default, but explicit for clarity
-  },
 })
